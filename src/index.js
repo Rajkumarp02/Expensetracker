4cd5e4790eac92fde8623ace70266aef9c5bc2ab
Rajkumarp02 <rajkumarprjpm@gmail.com>
2023-11-18 15:36:35 +0530

    Commit changes in my-client submodule

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware,compose } from 'redux';
+import thunk from 'redux-thunk';
+import Reducers from './Redux/store.js'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(Reducers,compose(applyMiddleware(thunk)))
+
+
 root.render(
+
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+   </React.StrictMode>
+   <ToastContainer/>
+   </Provider>
+   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
